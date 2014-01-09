dbcaa62e2ec7c251d96ea8407a9197dbbbd6d37c
Xorus <jdpepi@gmail.com>
2014-01-10 00:22:36 +0100

    Ajout d'une nouvelle structure pour les couleurs Suppression d'attributs inutiles Documentaiton

--- globals.hxx
+++ globals.hxx
@@ -20,12 +20,22 @@
 #include <vector> 
 #include <string> 
 
+/*! \namespace ChaseGame
+ * Project namespace for the game
+ */
 namespace ChaseGame {
+	//! Matrix line type
 	typedef std::vector<char> CVLine;
+	//! Matrix type
 	typedef std::vector<CVLine> CMatrix;
-	typedef std::pair<unsigned, unsigned> CPosition; // x, y
 
-	// Color value are defined in windows or unix
+	// Color value are defined in windows.hxx or unix.hxx
+
+	//! Language codes
+	enum Langs { LANG_FR, LANG_EN, LANG_ES };
+
+	//! Difficulty levels
+	enum DifficultyLevels { DIFFLVL_EASY, DIFFLVL_NORM, DIFFLVL_HARD, DIFFLVL_CRZY};
 
 	//! Character representing player one
 	const char KTokenPlayer1 = 'X';
@@ -40,10 +50,13 @@ namespace ChaseGame {
 	const int KColTokenP2 = CLR_BLUE;
 	
 	//! Character representing voidness
-	const char KEmpty = '.';
+	const char KEmpty = ' ';
 
-	//! Obstacles in the game
+	//! Character representing obstacles and walls
 	const char KObstacle = '#';
+
+	//! Character sent when a move is not allowed
+	const char KCancelled = '0';
 
 	//! Defines a position
 	struct SPlayerPos
@@ -52,6 +65,7 @@ namespace ChaseGame {
 		unsigned X = 0;
 		//! Y position
 		unsigned Y = 0;
+		//! Allow the comparison between to SPlayerPos
 		bool operator==(const SPlayerPos& objet) {
 		    return (X == objet.X) && (Y == objet.Y);
 		}
@@ -89,11 +103,26 @@ namespace ChaseGame {
 		SPlayerPos Position;
 		//! Keys specific to player
 		SPlayerKeys Keys;
+		//! Player score
+		int Score;
+		//! Player role : chasing or being chased
+		bool IsChasing;
 	};
 
-	struct SGameStatusDisplay {
-		int Time;
-		int PlayerChasing;
+	//! Color set to use when displaying the map
+	struct SColorSet {
+		//! Color value representing player one
+		int ColorP1;		
+		//! Color value representing player two
+		int ColorP2;		
+		//! Color value representing an obstacle
+		int ColorObstacle;
+		//! Color value representing a wall
+		int ColorWall; 
+		//! Color value representing a bonus
+		int ColorBonus;
+		//! Color value representing a malus 
+		int ColorMalus; 
 	};
 
 	//! Contains game configuration
@@ -106,16 +135,12 @@ namespace ChaseGame {
 		SPlayerState P1;
 		//! Player two status
 		SPlayerState P2;
+		//! Colors to use
+		SColorSet ColorSet;
+		//! Round
+		int Round;
 		//! Game lang
-		std::string Lang;
-
-		int Timer;
-
-		int PlayerChasing;
-
-		int Round; 
-
-
+		int Lang;
 	};
 }
 
