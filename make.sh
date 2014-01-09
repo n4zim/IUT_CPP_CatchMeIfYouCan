5c9067087e4e304a149141dd0687c0b230877cf4
Xorus <jdpepi@gmail.com>
2014-01-10 00:23:00 +0100

    Plus automatique que jamais.

--- make.sh
+++ make.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-g++ --std=c++11 -o projet unix.cxx windows.cxx file.cxx map.cxx main.cxx -Wall
+rm projet
+clear
+echo Starting compilation
+g++ --std=c++11 -o projet unix.cxx windows.cxx file.cxx map.cxx game.cxx main.cxx -Wall
+echo Program compiled, starting
 ./projet
