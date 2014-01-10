f046017abc5f529ce1b1d7697472e21fc1e28993
Xorus <jdpepi@gmail.com>
2014-01-10 16:41:27 +0100

    Ajout des états de jeu pour la musique

--- globals.hxx
+++ globals.hxx
@@ -39,6 +39,9 @@ namespace ChaseGame {
 	//! Unix color values
 	enum ConsoleColors { CLR_BLACK=30, CLR_RED=31, CLR_GREEN=32, CLR_YELLOW=33, CLR_BLUE=34, CLR_MAGENTA=35, CLR_CYAN=36, CLR_WHITE=37, CLR_GREY=30, CLR_RESET=0 };
 
+	//! Music state
+	enum GameMusicState { GMS_TITLE, GMS_STARTING, GMS_INGAME };
+
 	//! Character representing player one
 	const char KTokenPlayer1 = 'X';
 	
