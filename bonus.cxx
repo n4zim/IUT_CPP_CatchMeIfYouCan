65fce87b238c41c669d60dbffe240190288f8c96
Nazim Lachter <lachtater@gmail.com>
2014-01-10 17:49:26 +0100

    Ajout des fichiers de base pour les bonus et malus

---
+++ bonus.cxx
@@ -0,0 +1,28 @@
+/*!
+ *
+ * \file bonus.cxx
+ * \author 
+ * \date 10/01/14
+ * \brief File related bonus and malus
+ * \version 1
+ *
+ * Contains functions that creates bonuses and maluses in the map
+ * 
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <array>
+
+#include "globals.hxx"
+#include "map.hxx"
+
+using namespace std;
+
+namespace ChaseGame {
+
+
+
+}
---
+++ bonus.hxx
@@ -0,0 +1,21 @@
+/*!
+ * \file bonus.hxx
+ * \author 
+ * \date 10/01/14
+ * \brief bonus.cxx function prototypes
+ * \version 1
+ */
+
+#include <string>
+#include "globals.hxx"
+
+#ifndef bonus_h 
+#define bonus_h
+
+namespace ChaseGame {	
+
+
+
+}
+
+#endif
