589f116337c15e51add624071d7236fc435229df
Nazim Lachter <lachtater@gmail.com>
2014-01-11 00:24:53 +0100

    Ajout de la Super Banane !

---
+++ banana.hxx
@@ -0,0 +1,18 @@
+/*!
+ * \file banana.hxx
+ * \author Nazim'n'Josh
+ * \date 10/01/14
+ * \brief banana.cxx function prototypes
+ * \version 1
+ */
+
+#ifndef banana_h 
+#define banana_h
+
+namespace ChaseGame {
+
+	void SuperBanana ();
+
+}
+
+#endif
