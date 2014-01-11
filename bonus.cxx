2a34af4e73857380b6cc25b610d955d6a25a9633
Nazim Lachter <lachtater@gmail.com>
2014-01-11 18:04:40 +0100

    Mise à jour du fichier bonus

--- bonus.cxx
+++ bonus.cxx
@@ -22,37 +22,6 @@ using namespace std;
 
 namespace ChaseGame {
 
-	unsigned RRand (unsigned Min, unsigned Max)
-	{
-	    return rand() % (Max - Min) + Min;
-	}	
-
-	void GenBonus_Malus (CMatrix & Mat)
-	{
-		/*
-			{Bonus} Warp mur  			= ¤
-			{Bonus/Malus} Swap de rôle  = $
-			{Bonus} Stun  				= %
-			{Malus} Stun				= £
-			{Autres à voir...}
-		*/
-
-		string Bonus = "¤$%";
-		string Malus = "£";
-
-		unsigned y = RRand(0, Mat.size());
-		unsigned x = RRand(0, Mat[y].size());
-
-		if ((rand() % 100) > 98 && Mat[y][x] == KEmpty)
-		{
-			unsigned chance = RRand(0, 1);
-			if (chance == 0)
-				Mat[y][x] = Bonus[RRand(0, Bonus.size())];
-			else
-				Mat[y][x] = Malus[RRand(0, Malus.size())];
-		}
-	} // GenBonus_Malus ()
-
 	void Effect (CMatrix & Mat, SPlayerPos Player)
 	{
 		// A finir
