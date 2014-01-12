06419732b05f88f35d87ace99135fa8685b9ef0e
Xorus <jdpepi@gmail.com>
2014-01-12 21:49:09 +0100

    Changement mineur

--- make.sh
+++ make.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm projet *.o
 clear
 
 echo Starting compilation
@@ -7,5 +6,7 @@ g++ --std=c++11 -c unix.cxx audio.cxx file.cxx map.cxx game.cxx banana.cxx bonus
 
 echo Linking SFML 1.6 library
 g++ --std=c++11 -o projet unix.o audio.o file.o map.o game.o bonus.o banana.o main.o -lsfml-system -lsfml-audio
+
+rm unix.o audio.o file.o map.o game.o bonus.o banana.o main.o
 
 ./projet
